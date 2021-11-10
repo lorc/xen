d31218eaf1020f45bcb697655bfce1c58540c89b
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2021-11-10 11:41:43 +0200

    [SR-IOV] vpci: add SR-IOV support
    SR-IOV virtual functions have simplified configuration space such as
    Vendor ID is derived from the physical function and Device ID comes
    from SR-IOV extended capability.
    Emulate those, so we can provide VID/DID pair for guests which use PCI
    passthrough for SR-IOV virtual functions.
    
    Emulate guest BAR register values based on PF BAR values for VFs.
    This allows creating a guest view of the normal BAR registers and emulates
    the size and properties as it is done during PCI device enumeration by
    the guest.
    
    Note, that VFs ROM BAR is read-only and is all zeros, but VF may provide
    access to the PFs ROM via emulation and is not implemented.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- xen/drivers/vpci/header.c
+++ xen/drivers/vpci/header.c
@@ -447,8 +447,8 @@ static int modify_bars(const struct pci_dev *pdev, uint16_t cmd, bool rom_only)
 }
 
 /* TODO: Add proper emulation for all bits of the command register. */
-static void cf_check cmd_write(
-    const struct pci_dev *pdev, unsigned int reg, uint32_t cmd, void *data)
+void cf_check cmd_write(const struct pci_dev *pdev, unsigned int reg,
+                        uint32_t cmd, void *data)
 {
     uint16_t current_cmd = pci_conf_read16(pdev->sbdf, reg);
 
@@ -496,8 +496,8 @@ static uint32_t cf_check cmd_read(
     return pci_conf_read16(pdev->sbdf, reg);
 }
 
-static void cf_check bar_write(
-    const struct pci_dev *pdev, unsigned int reg, uint32_t val, void *data)
+void cf_check bar_write(const struct pci_dev *pdev, unsigned int reg,
+                        uint32_t val, void *data)
 {
     struct vpci_bar *bar = data;
     bool hi = false;
@@ -540,8 +540,8 @@ static void cf_check bar_write(
     pci_conf_write32(pdev->sbdf, reg, val);
 }
 
-static void cf_check guest_bar_write(
-    const struct pci_dev *pdev, unsigned int reg, uint32_t val, void *data)
+void cf_check guest_bar_write(const struct pci_dev *pdev, unsigned int reg,
+                              uint32_t val, void *data)
 {
     struct vpci_bar *bar = data;
     bool hi = false;
@@ -583,8 +583,8 @@ static void cf_check guest_bar_write(
     bar->guest_reg = guest_reg;
 }
 
-static uint32_t cf_check guest_bar_read(
-    const struct pci_dev *pdev, unsigned int reg, void *data)
+uint32_t cf_check guest_bar_read(const struct pci_dev *pdev, unsigned int reg,
+                                 void *data)
 {
     const struct vpci_bar *bar = data;
     bool hi = false;
@@ -678,6 +678,10 @@ static int cf_check init_bars(struct pci_dev *pdev)
 
     ASSERT(pcidevs_write_locked());
 
+    /* No need to init for virtual functions. */
+    if ( pdev->info.is_virtfn )
+        return 0;
+
     header = &pdev->vpci->header;
     bars = header->bars;
 
@@ -706,10 +710,10 @@ static int cf_check init_bars(struct pci_dev *pdev)
 
     /* Reset the command register for guests. */
     if ( !is_hwdom )
-        cmd_write(pdev, PCI_COMMAND, 0, header);
+        vpci_cmd_write(pdev, PCI_COMMAND, 0, header);
 
     /* Setup a handler for the command register. */
-    rc = vpci_add_register(pdev->vpci, cmd_read, cmd_write, PCI_COMMAND,
+    rc = vpci_add_register(pdev->vpci, cmd_read, vpci_cmd_write, PCI_COMMAND,
                            2, header);
     if ( rc )
         return rc;
@@ -731,8 +735,8 @@ static int cf_check init_bars(struct pci_dev *pdev)
         {
             bars[i].type = VPCI_BAR_MEM64_HI;
             rc = vpci_add_register(pdev->vpci,
-                                   is_hwdom ? vpci_hw_read32 : guest_bar_read,
-                                   is_hwdom ? bar_write : guest_bar_write,
+                                   is_hwdom ? vpci_hw_read32 : vpci_guest_bar_read,
+                                   is_hwdom ? vpci_bar_write : vpci_guest_bar_write,
                                    reg, 4, &bars[i]);
             if ( rc )
                 goto fail;
@@ -795,8 +799,8 @@ static int cf_check init_bars(struct pci_dev *pdev)
         bars[i].prefetchable = val & PCI_BASE_ADDRESS_MEM_PREFETCH;
 
         rc = vpci_add_register(pdev->vpci,
-                               is_hwdom ? vpci_hw_read32 : guest_bar_read,
-                               is_hwdom ? bar_write : guest_bar_write,
+                               is_hwdom ? vpci_hw_read32 : vpci_guest_bar_read,
+                               is_hwdom ? vpci_bar_write : vpci_guest_bar_write,
                                reg, 4, &bars[i]);
         if ( rc )
             goto fail;
